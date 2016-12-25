7a08d834c67968656100f443ae3da4940d6a984d
jkliebana <jcliebana@outlook.es>
2016-12-25 14:15:34 +0100

    Primer modelo y extraccion de datos

--- DiariosDeportivos/urls.py
+++ DiariosDeportivos/urls.py
@@ -13,6 +13,7 @@ urlpatterns = patterns('',
     url(r'^$', 'diarios_app.views.inicio', name='home'),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^noticias/$', 'diarios_app.views.noticias'),
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
--- diarios_app/views.py
+++ diarios_app/views.py
@@ -1,4 +1,9 @@
+from diarios_app.models import Noticia
 from django.shortcuts import render_to_response
 
 def inicio(request):
     return render_to_response('inicio.html')
+
+def noticias(request):
+    noticias = Noticia.objects.all()
+    return render_to_response('noticias.html', {'noticias': noticias})
