fcb16aab491d3f751c458735bb3f53b206fac380
jkliebana <jcliebana@outlook.es>
2017-01-04 23:12:20 +0100

    Adición de diario "EL MUNDO"

--- populate.py
+++ populate.py
@@ -10,7 +10,7 @@ from django.db import models
 from dateutil import parser
 from django.utils import timezone
 
-#Clase de pruebas
+#EXTRACCIÓN DE DATOS DE MARCA - FUTBOL
 @commit_on_success
 def read_primera_division_MARCA():
     print "Populando de Marca..."
@@ -77,6 +77,7 @@ def read_primera_division_MARCA():
                     print ""
 
 
+#EXTRACCIÓN DE DATOS DE AS - FUTBOL
 @commit_on_success
 def read_primera_division_AS():
     print "Populando de AS..."
@@ -138,19 +139,81 @@ def read_primera_division_AS():
                     print ""
 
 
+#EXTRACCIÓN DE DATOS DE EL MUNDO - FUTBOL
+@commit_on_success
+def read_primera_division_EL_MUNDO():
+    print "Populando de EL MUNDO..."
+    parseo = feedparser.parse('http://estaticos.elmundo.es/elmundodeporte/rss/futbol.xml')
+
+    #Tenemos que encontrar el ultimo id registrado
+    if len(Noticia.objects.all()) == 0:
+        counter = 1
+        lastDate = datetime.datetime.today()
+        lastDate = lastDate.replace(year=lastDate.year-3)
+    else:
+        if "EL MUNDO" not in Noticia.objects.values_list('procedente_de', flat=True):
+            counter = Noticia.objects.latest("id_noticia").id_noticia
+            counter += 1
+            lastDate = datetime.datetime.today()
+            lastDate = lastDate.replace(year=lastDate.year - 3)
+
+        else:
+            counter = Noticia.objects.latest("id_noticia").id_noticia
+            counter+=1
+            queryset = Noticia.objects.filter(procedente_de='EL MUNDO')
+            lastDate = queryset.latest("fecha").fecha
+            lastDate = lastDate.replace(hour=lastDate.hour + 1)
+
+    for entrada in parseo.entries:
+
+        revista = "EL MUNDO"
+        id = counter
+        counter+=1
+        tit = entrada.title
+        desc = entrada.content[0]['value']
+        desc = strip_tags(desc)
+        desc = desc[0:150]
+        url_not = entrada.link
+        foto = entrada.media_content[0]['url']
+
+        fecha = entrada.published_parsed
+        fech = fecha[0].__str__() + "-" + fecha[1].__str__() + "-" + fecha[2].__str__() + "-" + (fecha[3]).__str__() + ":" + fecha[4].__str__() + ":" + fecha[5].__str__()
+        fech2 = datetime.datetime.strptime(fech, "%Y-%m-%d-%H:%M:%S")
+        lastDateStr = lastDate.__str__()[0:19]
+        lastDate2 = datetime.datetime.strptime(lastDateStr.__str__(), "%Y-%m-%d %H:%M:%S")
+
+
+        if fech2 > lastDate2:
+            noticia = Noticia(id_noticia=id, titulo = tit, descripcion = desc, url_foto = foto, url_noticia = url_not, fecha = fech2, procedente_de=revista)
+            noticia.save()
+
+            for t in entrada.tags:
+                try:
+                    if t['term'] in Etiquetas.objects.values_list('nombre', flat=True):
+                        etiqueta = Etiquetas.objects.get(nombre=t['term'])
+                        etiqueta.noticias.add(noticia)
+                    else:
+                        id = len(Etiquetas.objects.all())
+                        name = t['term']
+                        etiqueta = Etiquetas(id_etiqueta=id, nombre=name)
+                        etiqueta.save()
+                        etiqueta.noticias.add(noticia)
+                except:
+                    print ""
+
+
+
 def prueba():
 
-    etiquetas = Etiquetas.objects.values_list('nombre', flat=True)
+    news = Noticia.objects.all().order_by("-fecha")
 
-    if "AS" not in Noticia.objects.values_list('procedente_de', flat=True):
-        print "YEP"
-    else:
-        print "np"
+    for n in news:
+        print n.fecha
 
 
 
 if __name__ == "__main__":
     read_primera_division_MARCA()
     read_primera_division_AS()
-
+    read_primera_division_EL_MUNDO()
     #prueba()
