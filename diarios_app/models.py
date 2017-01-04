904f01e3d01e882de82576bdc90ce6c6663d6dce
jkliebana <jcliebana@outlook.es>
2017-01-04 14:20:47 +0100

    Arreglo de populate.py para añadir automaticamente nuevas noticias según fecha

--- diarios_app/models.py
+++ diarios_app/models.py
@@ -5,24 +5,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Noticia(models.Model):
-    id_noticia = models.IntegerField(unique=True)
+    id_noticia = models.IntegerField(unique=True, primary_key=True)
     titulo = models.CharField(max_length=50)
     descripcion = models.CharField(max_length=250)
     url_foto = models.URLField()
     url_noticia = models.URLField()
-    fecha = models.DateField()
+    fecha = models.DateTimeField()
     procedente_de = models.CharField(max_length=25)
 
     def __unicode__(self):
         return self.titulo
-'''
-class Usuario(models.Model):
-    id_usuario = models.IntegerField(unique=True)
-    nombre = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.nombre'''
 
 class Puntuacion(models.Model):
     id_noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE)
--- populate.py
+++ populate.py
@@ -2,20 +2,31 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'DiariosDeportivos.settings'
 import feedparser
+import datetime
 from django.db.transaction import commit_on_success
 from diarios_app.models import Noticia
 from django.utils.html import strip_tags
-
+from django.db import models
+from dateutil import parser
+from django.utils import timezone
 
 #Clase de pruebas
 @commit_on_success
-def read_test():
+def read_primera_division_MARCA():
     print "Populando de Marca..."
     parseo = feedparser.parse('http://estaticos.marca.com/rss/futbol/primera-division.xml')
 
-    counter = 0
+    #Tenemos que encontrar el ultimo id registrado
+    if Noticia.objects.latest("id_noticia").id_noticia <= 0:
+        counter = 1
+    else:
+        counter = Noticia.objects.latest("id_noticia").id_noticia
+        counter+=1
+        lastDate = Noticia.objects.latest("fecha").fecha
+        lastDate = lastDate.replace(hour=lastDate.hour + 1)
+
     for entrada in parseo.entries:
-        print "Populando " + counter.__str__()
+
         id = counter
         counter+=1
         tit = entrada.title
@@ -24,12 +35,28 @@ def read_test():
         url_not = entrada.link
         foto = entrada.media_content[0]['url']
         fecha = entrada.published_parsed
-        fech = fecha[0].__str__() + "-" + fecha[1].__str__() + "-" + fecha[2].__str__()
+        fech = fecha[0].__str__() + "-" + fecha[1].__str__() + "-" + fecha[2].__str__() + "-" + (fecha[3]+1).__str__() + ":" + fecha[4].__str__() + ":" + fecha[5].__str__()
         revista = "MARCA"
 
-        noticia = Noticia(id_noticia=id, titulo = tit, descripcion = desc, url_foto = foto, url_noticia = url_not, fecha = fech, procedente_de=revista)
-        noticia.save()
+        fech2 = datetime.datetime.strptime(fech, "%Y-%m-%d-%H:%M:%S")
+
+        lastDateStr = lastDate.__str__()[0:19]
+        lastDate2 = datetime.datetime.strptime(lastDateStr.__str__(), "%Y-%m-%d %H:%M:%S")
+
+
+        if fech2 > lastDate2:
+            noticia = Noticia(id_noticia=id, titulo = tit, descripcion = desc, url_foto = foto, url_noticia = url_not, fecha = fech2, procedente_de=revista)
+            noticia.save()
+
+def prueba():
+    print "Ejecutando prueba..."
+
+    news = Noticia.objects.all()
+    lastDate = Noticia.objects.latest("fecha").fecha
+    lastDate = lastDate.replace(hour=lastDate.hour + 1)
+    print lastDate.__str__()
 
 
 if __name__ == "__main__":
-    read_test()
+    read_primera_division_MARCA()
+    #prueba()
